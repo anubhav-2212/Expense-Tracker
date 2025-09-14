4383e351bf59afe6430dc79ae34278c7f5fb1b14
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2025-09-14 14:42:21 +0530

    Landing Page Completed

--- expense-tracker/app/_components/Header.jsx
+++ expense-tracker/app/_components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='p-2.5 flex items-center justify-between border shadow-sm' >
+    <div className='p-2.5 flex items-center justify-between border shadow-sm mb-10' >
         <div>
      <Image src="/expenses (1).png" alt="Expenses logo" width={50  } height={15}  />
      <span className='font-bold font-sans '>Expense Tracker</span>
--- expense-tracker/app/_components/Hero.jsx
+++ expense-tracker/app/_components/Hero.jsx
@@ -1,10 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-        <section className="bg-white lg:grid lg:place-content-center">
-  <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+    <div className='flex flex-col items-center'>
+        <section className="bg-white lg:grid  lg:place-content-center">
+  {/* <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32"> */}
+  <div className="mx-auto w-screen max-w-screen-xl px-4 pb-10 sm:px-6 sm:pb-12 lg:px-8 lg:pb-32">
+
     <div className="mx-auto max-w-prose text-center">
       <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
         Manage Your Expenses
@@ -18,7 +21,7 @@ const Hero = () => {
 
       <div className="mt-4 flex justify-center gap-4 sm:mt-6">
         <a
-          className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
+          className="inline-block rounded border border-indigo-500 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-black hover:border-black"
           href="#"
         >
           Get Started
@@ -29,6 +32,7 @@ const Hero = () => {
     </div>
   </div>
 </section>
+<Image src="/dashboard.webp" alt="dashboard" width={1000} height={500} className=' rounded-xl border-2' />
       
     </div>
   )
